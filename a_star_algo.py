514328b6e2fa55276eb9f3c045eef58f710fa9fd
shranav <shravankandlakunta@gmail.com>
2020-11-04 03:52:05 -0500

    Updating method skeletons

--- a_star_algo.py
+++ a_star_algo.py
@@ -1,10 +1,20 @@
 from queue import PriorityQueue as pq
 import math
 from flask import Flask
+from node_object import Node
 
 
-def a_star_algo():
-    open_nodes = []
+open_nodes = []
+node_objects_list = []
+# Initialize priority queue
+
+
+def a_star():
+    pass
+
+
+def process_input(json_file):
+    # TODO create objects of walls, start, and end
     pass
 
 
@@ -20,29 +30,31 @@ def h_heuristic(curr_node, end_node):
        end_node (node object) - target node
     """
 
-    curr_node.h = math.sqrt(((end_node.x - curr_node.x)**2) + ((end_node.y - curr_node.y)**2)))
+    curr_node.h = math.sqrt(((end_node.x - curr_node.x) ** 2) + ((end_node.y - curr_node.y) ** 2))
 
 
-def g_heuristic():
-    """Calculates cost
+def g_heuristic(current_n):
+    """Calculates cost of traveling from the starting node to current node
 
-       Used to calculate the distance between two points
-       on a graph, which will be used to determine the f
+       Used to calculate the current cost of traveling to the current nodes
+       from the starting node, which will be used to determine the f
        heuristic in the A* algorithm
 
        :arg
-
-       :return
-       h (int) - calculated value
+       current_n (node object) - current node being checked
     """
 
-    g = 0
+    # TODO need to fix method logic
+    current_n.g += current_n.last_node.g  # Don't know if this works
 
-    return g
 
+def f_heuristic(current_node):
+    """Actual heuristic used to compare the optimal paths between nodes
 
-def f_heuristic():
-    pass
+       :arg
+       current_n (node object) - current node being checked
+    """
+    current_node.f = current_node.g + current_node.h
 
 
 def save_state():
